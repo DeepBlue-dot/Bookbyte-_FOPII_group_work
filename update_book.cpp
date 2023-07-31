beadc8e581b8f44a673cd3515c8f6facd0782464
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2023-07-31 14:30:49 +0300

    Update update_book.cpp
    functionality update

--- update_book.cpp
+++ update_book.cpp
@@ -1,21 +1,26 @@
 #include "header.h"
-
 void update_book(int& num, Book books[])
 {
     int n;
     char status;
     string due_date;
-    void borrowers(int &num, Book books[]);
-    cout<<"Choose a book and enter its book number ";
+    void browse(int &num, Book books[]);
+    cout<<"Choose a book to borrow and enter its book number ";
     cin>>n;
     if (n>=1 && n < num)
     {
-      cout<<" choose 1 to borrow or else to cancel"<<endl ;
+      cout<<" choose: \n1 to update status to borrowed \n2 to returned \nelse to cancel"<<endl ;
       cin>>status;
-      if (status=='1');
+      if (status=="1")//to borrow
       {
-           books[n-1].status="borrowed";
+           books[n-1].status="Borrowed";
            getline(cin,due_date);
+           cout<<"\nUpdate successful";
+      }
+      else if(status=="2")//to return
+      {
+        books[n-1].status="Available";
+        cout<<"\nUpdate successful";
       }
       else
         cout<<"\nUpdate Cancelled";
