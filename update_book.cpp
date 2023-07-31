6757221e63c7fb18c21cf1379047d51c465e7a13
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2023-07-31 13:54:06 +0300

    update function
    allows to update status and due date of book

--- update_book.cpp
+++ update_book.cpp
@@ -2,5 +2,30 @@
 
 void update_book(int& num, Book books[])
 {
-    
+    int n;
+    char status;
+    string due_date;
+    void borrowers(int &num, Book books[]);
+    cout<<"Choose a book and enter its book number ";
+    cin>>n;
+    if n < num
+    {
+      cout<<" choose 1 to borrow or else to cancel"<<endl ;
+      cin>>status;
+      if status=='1';
+      {
+           books[n-1].status="borrowed"
+           getline(cin,due_date);
+      }
+      else
+        cout<<"\nUpdate Cancelled";
+    }
+
+    else
+    {
+     cout<<" Wrong entry ";
+    }
+
+   void save_books(int& num, Book books[])
 }
+ 
